1fd86710249578fd134ad7ff3025ad1b32f83ab9
Pixel-0 <tiny0pixel@gmail.com>
2019-06-17 15:26:50 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,64 @@
+# The Pizza Peddler
+
+
+#### By Sara Munini
+
+## Description
+
+This program allow a user to create a custom pizza and shows the total of their order in a Cart. The user will also be prompted to enter their location if they want the pizza delivered for them.
+
+## Setup/Installation Requirements
+
+This program can only be accessed on a computer with Git and Atom installed.
+
+* Open your terminal
+* initialize git (git init)
+* git clone https://github.com/Pixel-0/delani-studio.git
+* cd delani-studio
+* open folder in your preferred IDE
+* proceed to open index.html file using a browser of your choice.
+
+
+## Known Bugs
+
+No known bugs.
+
+## Specifications
+
+The program should... | Example Input | Example Output
+----- | ----- | -----
+Calculate the price of a custom pizza based on the options selected in the form | Custom Pizza Form: Size-Small, 10"; Sauce-Alfredo; Cheese-Cheese; Veggie 1-Onions; Veggie 2-Green Peppers; Meat-Sausage | $12
+Display the total price of the custom pizza in the Cart column, and simultaneously refresh the form | Custom Pizza Form: Size-Small, 10"; Sauce-Alfredo; Cheese-Cheese; Veggie 1-Onions; Veggie 2-Green Peppers; Meat-Sausage, Cart: Price $-- | Cart: Price $12
+Add $3 to the Cart when a Sides button is clicked | Cart: Price $--, Sides: Italian Soda | Cart: Price $3
+A link with the item name will appear when an item is added to the Cart | Cart: --, Sides: Brownie | Cart: Sides, clicked on reveals "1 jumbo, double-chocolate brownie"
+Details will append in list form under the proper food category in the Cart | Cart: Sides - "3 garlic breadsticks", Sides: Breadsticks  | Cart: Sides - "3 garlic breadsticks", "3 garlic breadsticks"
+
+## Future Features
+
+HTML | CSS | JavaScript
+----- | ----- | -----
+--- | Make Orders page responsive | Add default/pre-made pizza buttons to Orders page-header
+--- | Add high-resolution pictures of food to Orders page | Refactor repetitive code in UI Logic
+
+## Support and Contact Details
+
+Contact Epicodus for support in running this program.
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* Font Awesome
+* jQuery
+* JavaScript
+
+## Links
+
+Git Hub Webpage: https://pullashradha.github.io/PizzaOrder
+
+### License
+
+*This software is licensed under the GPL license.*
+
+Copyright (c) 2016 Shradha Pulla
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-$("#order-details").hide();
-$("#deliver").hide();
+  $("#order-details").hide();
+  $("#deliver").hide();
 
   // Business Logic
   var totalPriceArray = [];
-  function Order (size, crust, toppings) {
+
+  function Order(size, crust, toppings) {
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
     this.pizzaPrice = 0;
   }
 
-  Order.prototype.pizzaCost = function () {
+  Order.prototype.pizzaCost = function() {
     if (this.size === "small-pizza") {
       this.pizzaPrice += 500;
     } else if (this.size === "medium-pizza") {
@@ -23,9 +24,9 @@ $("#deliver").hide();
       this.pizzaPrice += 100;
     } else if (this.crust === "thick") {
       this.pizzaPrice += 150;
-    }else if (this.crust === "stuffed") {
+    } else if (this.crust === "stuffed") {
       this.pizzaPrice += 150;
-    }else if (this.crust === "crispy") {
+    } else if (this.crust === "crispy") {
       this.pizzaPrice += 150;
     }
 
@@ -33,58 +34,63 @@ $("#deliver").hide();
       this.pizzaPrice += 100;
     } else if (this.toppings === "sausage") {
       this.pizzaPrice += 150;
-    }else if (this.toppings === "bacon") {
+    } else if (this.toppings === "bacon") {
       this.pizzaPrice += 200;
-    }else if (this.toppings === "mushrooms") {
+    } else if (this.toppings === "mushrooms") {
       this.pizzaPrice += 150;
-    }else if (this.toppings === "chicken") {
+    } else if (this.toppings === "chicken") {
       this.pizzaPrice += 150;
     }
 
   };
 
-  function Address (address) {
-  this.address = address;
-  this.deliveryAddress = (address);
-}
+  function Address(address) {
+    this.address = address;
+    this.deliveryAddress = (address);
+  }
 
-  Order.prototype.finalCost = function () {
+  Order.prototype.finalCost = function() {
     var cartTotalPrice = [];
-    for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement ++) {
+    for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement++) {
       cartTotalPrice += totalPriceArray[arrayElement];
     }
     return cartTotalPrice;
   };
 
+  $(".btn.check-out").click(function() {
+
+  });
+
   $("form#custom-pizza").submit(function(event) {
-      event.preventDefault();
-      var size = $("select#size").val();
-      var crust = $("select#crust").val();
-      var toppings = $("select#toppings").val();
-      var pizzaDetails = (size + " - " + crust + " - " + toppings);
-      var newPizzaOrder = new Order(size, crust, toppings);
-      newPizzaOrder.pizzaCost();
-      totalPriceArray.push(newPizzaOrder.pizzaPrice);
-      // $("#pizza-details").hide();
-      $("#final-cost").text(newPizzaOrder.finalCost());
-      $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
-      // $("#size, #crust, #toppings,").val("");
-    });
+    event.preventDefault();
+    var size = $("select#size").val();
+    var crust = $("select#crust").val();
+    var toppings = $("select#toppings").val();
+    var pizzaDetails = (size + " - " + crust + " - " + toppings);
+    var newPizzaOrder = new Order(size, crust, toppings);
+    newPizzaOrder.pizzaCost();
+    totalPriceArray.push(newPizzaOrder.pizzaPrice);
+    // $("#pizza-details").hide();
+    $("#final-cost").text(newPizzaOrder.finalCost());
+    $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
+    // $("#size, #crust, #toppings,").val("");
+  });
 
-    $("#submit-pizza").click(function(){
-      $("#deliver").toggle();
-    });
+  $("#submit-pizza").click(function() {
+    $("#deliver").toggle();
+  });
 
-    $("#checkout-btn").click(function() {
-      $("#order-details").toggle();
-    });
+  $("#checkout-btn").click(function() {
+    $("#order-details").toggle();
+  });
 
-    $("form#address-form").submit(function(event) {
-      $(".address-form").toggle();
-      event.preventDefault();
-      var address = $("input#location").val();
-      var newAddress = new Address(address);
+  $("form#address-form").submit(function(event) {
+    $(".address-form").toggle();
+    event.preventDefault();
+    var address = $("input#location").val();
+    var newAddress = new Address(address);
 
-      $("#delivery-option").text("Deliver to: " + newAddress.deliveryAddress);
-    });
+    $("#delivery-option").text("Deliver to: " + newAddress.deliveryAddress);
+  });
+
 });
